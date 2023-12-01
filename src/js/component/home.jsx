63519ea62790f2df66bb20f8f7ca0726bd37f53b
Braiton57 <143307681+Braiton57@users.noreply.github.com>
2023-12-01 17:30:43 +0000

    to do list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,45 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [tarea,setTarea]=useState("")
-	const [lista,setLista]=useState([])
-	function agregarTarea(){
+	const [tarea, setTarea] = useState("")
+	const [lista, setLista] = useState([])
+	function agregarTarea(e) {
 		e.preventDefault()
-		setLista([...lista,tarea])
+		setLista([...lista, tarea])
 		setTarea("")
 	}
+function eliminar(id){
+	let x=[]
+	x= lista.filter((item,index)=>{
+		if(index!=id){
+			return item
+		}
+	})
+	setLista(x)
+}
 	return (
 		<div className="text-center container">
 
-			<input className="form-control" type="text" value={tarea} onChange={(e)=>setTarea(e.target.value)} />
+			<input className="form-control" type="text" value={tarea} onChange={(e) => setTarea(e.target.value)} />
 
 			<button className="btn btn-success" onClick={agregarTarea}>tarea</button>
+			<div>
+				<ul className="list-group">
+					{lista.map((item, id) => (
+						<li className="list-group-item" key={id}>
+							{item}
+							<button className="btn btn-danger float-end"onClick={()=>eliminar (id)} type="button">
+								x
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
 
-	
 		</div>
 	);
 };
